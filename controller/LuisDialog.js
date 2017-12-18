cc07b65c95e9b707293c46bf6c5e7eeb522232c0
Raghav Garg <raghavgarg32@gmail.com>
2017-12-19 09:53:36 +1300

    update

---
+++ controller/LuisDialog.js
@@ -0,0 +1,79 @@
+var builder = require('botbuilder');
+var food = require("./ModifyDetails");
+var details = require("./ModifyDetails");
+var currency = require('./Currency');
+var weather = require('./Weather');
+
+var qna = require('./QnAMaker');
+var correctlogin = require("./ModifyDetails");
+
+// Some sections have been omitted
+var isAttachment = false;
+
+
+exports.startDialog = function (bot) {
+    // Replace {YOUR_APP_ID_HERE} and {YOUR_KEY_HERE} with your LUIS app ID and your LUIS key, respectively.
+    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f5c7f697-cd55-415b-97bf-cd59690104ed?subscription-key=d815d5827cc84b4b8e9935674293bd02&verbose=true&timezoneOffset=0&q=');
+    
+    bot.recognizer(recognizer);
+  
+  
+bot.dialog('Start', [//Coverts currency
+    function (session, args, next) {
+        session.dialogData.args = args || {};        
+        builder.Prompts.text(session, "Please enter in the city you would like to know the weather for?"); //Asks the user for the amount
+            next(); // Skip if we already have this info.
+    },
+    function (session, result, next) {
+        session.conversationData["city"] = result.response;//Stores the amount in amount
+            next();
+            // <---- THIS LINE HERE IS WHAT WE NEED 
+        
+    },
+        function (session, result, next) {
+        weather.displayWeather(session, session.conversationData["city"]); //Shows the user the conversion
+        next();
+    },
+    function (session, result, next) {
+        session.conversationData["city"] = result.response;//Stores the amount in amount
+        // <---- THIS LINE HERE IS WHAT WE NEED 
+        next();
+        
+        
+    },
+  
+    
+
+]).triggerAction({
+    matches: 'Start'//when this intent is picked up it triggers the function
+});  
+
+bot.dialog('WelcomeIntent', [//Coverts currency
+    function (session, args, next) {
+        session.dialogData.args = args || {};        
+        builder.Prompts.text(session, "Would you like to see the weather of your current city?"); //Asks the user for the amount
+    },
+    
+]).triggerAction({
+    matches: 'WelcomeIntent'//when this intent is picked up it triggers the function
+});  
+
+bot.dialog('End', [//Coverts currency
+    function (session, args, next) {
+        session.dialogData.args = args || {};        
+        builder.Prompts.text(session, "Thats it folks!!!"); //Asks the user for the amount
+    },
+    
+]).triggerAction({
+    matches: 'End'//when this intent is picked up it triggers the function
+});  
+
+}
+
+
+
+
+
+   
+
+
