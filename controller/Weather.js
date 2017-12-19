b8ddf3691e00dca255accb0de1f3c07e0f797b96
simonehalstead <simone-halstead@hotmail.com>
2017-12-19 16:57:37 +1300

    added a hero card for different types of weather

--- controller/Weather.js
+++ controller/Weather.js
@@ -10,9 +10,70 @@ exports.displayWeather = function getWeatherData(session, city) {
 //This displays the weather
 function handleWeatherResponse(message, session, city) {
     var weatherResponse = JSON.parse(message); //Parses JSON data
-    if (weatherResponse.weather[0]) {
-        session.send("The weather in " + city + " is: " + weatherResponse.weather[0].main);
+    let imageurl;
+
+    if (weatherResponse.weather[0].main.toLowerCase() === "clear"){
+    	imageurl = "https://icons.wxug.com/data/wximagenew/k/Klockheed/2.jpg";
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "smoke"){
+    	imageurl = "https://cdn.pixabay.com/photo/2016/10/19/12/38/industry-1752876_960_720.png";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "rain"){
+    	imageurl = "https://i.ytimg.com/vi/J5OSRpRyl6g/maxresdefault.jpg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "sunny"){
+    	imageurl = "http://www.qygjxz.com/data/out/217/5720389-sunny-pictures.jpg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "snow"){
+    	imageurl = "http://i.funny.pho.to/preview/snow_effect/falling_snow_effect.jpeg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "drizzle"){
+    	imageurl = "https://c1.staticflickr.com/8/7393/11375147803_0f453d8676_b.jpg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "thunder"){
+    	imageurl = "https://cdn.pixabay.com/photo/2017/02/13/20/50/thunder-2063728_960_720.jpg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "fog"){
+    	imageurl = "https://img00.deviantart.net/5b37/i/2016/006/a/0/fog_stock_05_by_malleni_stock-d9mylkv.jpg";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "clouds"){
+    	imageurl = "http://www.agweek.com/sites/default/files/styles/full_1000/public/field/image/clouds-1571768_1280.jpg?itok=IDXqFiJK";
+
+    }
+    else if (weatherResponse.weather[0].main.toLowerCase() === "mist" || weatherResponse.weather[0].main.toLowerCase() === "haze"){
+    	imageurl = "https://prodwpids-idrivesafely.netdna-ssl.com/wp-content/uploads/2015/01/DRIVING_IN_THE_MIST.jpg";
 
     }
 
+
+
+
+
+    if (weatherResponse.weather[0]) {
+        //session.send("The weather in " + city + " is: " + weatherResponse.weather[0].main);
+        var msg = new builder.Message(session);
+	    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+	    msg.attachments([
+	        new builder.HeroCard(session)
+	            .title("Weather For " + city)
+	            
+	            .text(weatherResponse.weather[0].main)
+	            .images([builder.CardImage.create(session, imageurl)])
+	            
+	      
+	            
+	            
+	    ]);
+	    session.send(msg);
+	
+
+
+    }
 }
